a02ad5a57c57074e201b544180d245ed6cbd332d
Kevin Waldock <kevin.waldock@gmail.com>
2018-01-08 10:31:00 +0100

    Update...

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-from bottle import route, run, template, static_file
+from bottle import route, run, template, static_file, Bottle
 
-
-@route('/')
-def root():
-    return template('index.tpl')
-
-@route('/upload_gif')
-def upload_gif():
-    return template('upload_gif')
-
-@route('/view_gif')
-def view_gif():
-    return template('view_gif')
-
-@route('/remove_gif')
-def remove_gif():
-    return template('remove_gif')
-
+from memedatabase import MemeDatabase
 
 if __name__ == '__main__':
-    run(host='localhost', port=8080)
+    app = application = Bottle()
+    db = MemeDatabase()
+
+    @app.route('/static/images/<picture>')
+    def serve_pictures(picture):
+        return static_file(picture, root='views/static/images')
+
+    @app.route('/static/css/<css>')
+    def css_pictures(css):
+        return static_file(css, root='views/static/css')
+
+    @app.route('/')
+    def root():
+        return template('index.tpl')
+
+
+    @app.route('/upload_gif')
+    def upload_gif():
+        return template('upload_gif')
+
+
+    @app.route('/view_gif')
+    def view_gif():
+        return template('view_gif')
+
+
+    @app.route('/remove_gif')
+    def remove_gif():
+        return template('remove_gif')
+
+    @app.error(404)
+    def error404(error):
+        return template('404')
+
+    app.run(host='localhost', port=8080)
---
+++ views/404.tpl
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>The global GIF Database</title>
+</head>
+<body>
+
+<div style="text-align: center">
+    <p>Sorry, this is not a valid page</p>
+    <p>Enjoy this gif</p>
+    <img src="static/images/Peanut-butter-jelly-time.gif" alt="jelly">
+    <p>Or return to the <a href="/"> main page</a></p>
+</div>
+
+</body>
+</html>
